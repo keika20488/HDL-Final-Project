689be0fea13a4c3c0e0c5007178432308a96888a
keika20488 <emma990091@gmail.com>
2023-12-14 18:29:39 +0800

    Create template.v

---
+++ template.v
@@ -0,0 +1,401 @@
+module clock_divider #(
+    parameter n = 27
+)(
+    input wire  clk,
+    output wire clk_div  
+);
+
+    reg [n-1:0] num;
+    wire [n-1:0] next_num;
+
+    always @(posedge clk) begin
+        num <= next_num;
+    end
+
+    assign next_num = num + 1;
+    assign clk_div = num[n-1];
+endmodule
+
+module debounce (
+	input wire clk,
+	input wire pb, 
+	output wire pb_debounced 
+);
+	reg [3:0] shift_reg; 
+
+	always @(posedge clk) begin
+		shift_reg[3:1] <= shift_reg[2:0];
+		shift_reg[0] <= pb;
+	end
+
+	assign pb_debounced = ((shift_reg == 4'b1111) ? 1'b1 : 1'b0);
+
+endmodule
+
+module one_pulse (
+    input wire clk,
+    input wire pb_in,
+    output reg pb_out
+);
+
+	reg pb_in_delay;
+
+	always @(posedge clk) begin
+		if (pb_in == 1'b1 && pb_in_delay == 1'b0) begin
+			pb_out <= 1'b1;
+		end else begin
+			pb_out <= 1'b0;
+		end
+	end
+	
+	always @(posedge clk) begin
+		pb_in_delay <= pb_in;
+	end
+endmodule
+
+module SevenSegment(
+    output reg [6:0] display,
+    output reg [3:0] digit,
+    input wire [15:0] nums,
+    input wire rst,
+    input wire clk
+);
+    
+    reg [15:0] clk_divider;
+    reg [3:0] display_num;
+    
+    always @ (posedge clk, posedge rst) begin
+        if (rst) begin
+            clk_divider <= 15'b0;
+        end else begin
+            clk_divider <= clk_divider + 15'b1;
+        end
+    end
+    
+    always @ (posedge clk_divider[15], posedge rst) begin
+        if (rst) begin
+            display_num <= 4'b0000;
+            digit <= 4'b1111;
+        end else begin
+            case (digit)
+                4'b1110 : begin
+                        display_num <= nums[7:4];
+                        digit <= 4'b1101;
+                    end
+                4'b1101 : begin
+                        display_num <= nums[11:8];
+                        digit <= 4'b1011;
+                    end
+                4'b1011 : begin
+                        display_num <= nums[15:12];
+                        digit <= 4'b0111;
+                    end
+                4'b0111 : begin
+                        display_num <= nums[3:0];
+                        digit <= 4'b1110;
+                    end
+                default : begin
+                        display_num <= nums[3:0];
+                        digit <= 4'b1110;
+                    end				
+            endcase
+        end
+    end
+    
+    always @ (*) begin
+        case (display_num)
+            0 : display = 7'b1000000;	//0000
+            1 : display = 7'b1111001;   //0001                                                
+            2 : display = 7'b0100100;   //0010                                                
+            3 : display = 7'b0110000;   //0011                                             
+            4 : display = 7'b0011001;   //0100                                               
+            5 : display = 7'b0010010;   //0101                                               
+            6 : display = 7'b0000010;   //0110
+            7 : display = 7'b1111000;   //0111
+            8 : display = 7'b0000000;   //1000
+            9 : display = 7'b0010000;	//1001
+            10: display = 7'b0111111;   //'-'
+            default : display = 7'b1111111;
+        endcase
+    end
+    
+endmodule
+
+
+module vga_controller (
+    input wire pclk, reset,
+    output wire hsync, vsync, valid,
+    output wire [9:0]h_cnt,
+    output wire [9:0]v_cnt
+);
+
+    reg [9:0]pixel_cnt;
+    reg [9:0]line_cnt;
+    reg hsync_i,vsync_i;
+
+    parameter HD = 640;
+    parameter HF = 16;
+    parameter HS = 96;
+    parameter HB = 48;
+    parameter HT = 800; 
+    parameter VD = 480;
+    parameter VF = 10;
+    parameter VS = 2;
+    parameter VB = 33;
+    parameter VT = 525;
+    parameter hsync_default = 1'b1;
+    parameter vsync_default = 1'b1;
+
+    always @(posedge pclk)
+        if (reset)
+            pixel_cnt <= 0;
+        else
+            if (pixel_cnt < (HT - 1))
+                pixel_cnt <= pixel_cnt + 1;
+            else
+                pixel_cnt <= 0;
+
+    always @(posedge pclk)
+        if (reset)
+            hsync_i <= hsync_default;
+        else
+            if ((pixel_cnt >= (HD + HF - 1)) && (pixel_cnt < (HD + HF + HS - 1)))
+                hsync_i <= ~hsync_default;
+            else
+                hsync_i <= hsync_default; 
+
+    always @(posedge pclk)
+        if (reset)
+            line_cnt <= 0;
+        else
+            if (pixel_cnt == (HT -1))
+                if (line_cnt < (VT - 1))
+                    line_cnt <= line_cnt + 1;
+                else
+                    line_cnt <= 0;
+
+    always @(posedge pclk)
+        if (reset)
+            vsync_i <= vsync_default; 
+        else if ((line_cnt >= (VD + VF - 1)) && (line_cnt < (VD + VF + VS - 1)))
+            vsync_i <= ~vsync_default; 
+        else
+            vsync_i <= vsync_default; 
+
+    assign hsync = hsync_i;
+    assign vsync = vsync_i;
+    assign valid = ((pixel_cnt < HD) && (line_cnt < VD));
+
+    assign h_cnt = (pixel_cnt < HD) ? pixel_cnt : 10'd0;
+    assign v_cnt = (line_cnt < VD) ? line_cnt : 10'd0;
+
+endmodule
+
+module KeyboardDecoder(
+	output reg [511:0] key_down,
+	output wire [8:0] last_change,
+	output reg key_valid,
+	inout wire PS2_DATA,
+	inout wire PS2_CLK,
+	input wire rst,
+	input wire clk
+);
+    
+    parameter [1:0] INIT			= 2'b00;
+    parameter [1:0] WAIT_FOR_SIGNAL = 2'b01;
+    parameter [1:0] GET_SIGNAL_DOWN = 2'b10;
+    parameter [1:0] WAIT_RELEASE    = 2'b11;
+    
+	parameter [7:0] IS_INIT			= 8'hAA;
+    parameter [7:0] IS_EXTEND		= 8'hE0;
+    parameter [7:0] IS_BREAK		= 8'hF0;
+    
+    reg [9:0] key;		// key = {been_extend, been_break, key_in}
+    reg [1:0] state;
+    reg been_ready, been_extend, been_break;
+    
+    wire [7:0] key_in;
+    wire is_extend;
+    wire is_break;
+    wire valid;
+    wire err;
+    
+    wire [511:0] key_decode = 1 << last_change;
+    assign last_change = {key[9], key[7:0]};
+    
+    KeyboardCtrl_0 inst (
+		.key_in(key_in),
+		.is_extend(is_extend),
+		.is_break(is_break),
+		.valid(valid),
+		.err(err),
+		.PS2_DATA(PS2_DATA),
+		.PS2_CLK(PS2_CLK),
+		.rst(rst),
+		.clk(clk)
+	);
+	
+	one_pulse op (
+		.pb_out(pulse_been_ready),
+		.pb_in(been_ready),
+		.clk(clk)
+	);
+    
+    always @ (posedge clk, posedge rst) begin
+    	if (rst) begin
+    		state <= INIT;
+    		been_ready  <= 1'b0;
+    		been_extend <= 1'b0;
+    		been_break  <= 1'b0;
+    		key <= 10'b0_0_0000_0000;
+    	end else begin
+    		state <= state;
+			been_ready  <= been_ready;
+			been_extend <= (is_extend) ? 1'b1 : been_extend;
+			been_break  <= (is_break ) ? 1'b1 : been_break;
+			key <= key;
+    		case (state)
+    			INIT : begin
+    					if (key_in == IS_INIT) begin
+    						state <= WAIT_FOR_SIGNAL;
+    						been_ready  <= 1'b0;
+							been_extend <= 1'b0;
+							been_break  <= 1'b0;
+							key <= 10'b0_0_0000_0000;
+    					end else begin
+    						state <= INIT;
+    					end
+    				end
+    			WAIT_FOR_SIGNAL : begin
+    					if (valid == 0) begin
+    						state <= WAIT_FOR_SIGNAL;
+    						been_ready <= 1'b0;
+    					end else begin
+    						state <= GET_SIGNAL_DOWN;
+    					end
+    				end
+    			GET_SIGNAL_DOWN : begin
+						state <= WAIT_RELEASE;
+						key <= {been_extend, been_break, key_in};
+						been_ready  <= 1'b1;
+    				end
+    			WAIT_RELEASE : begin
+    					if (valid == 1) begin
+    						state <= WAIT_RELEASE;
+    					end else begin
+    						state <= WAIT_FOR_SIGNAL;
+    						been_extend <= 1'b0;
+    						been_break  <= 1'b0;
+    					end
+    				end
+    			default : begin
+    					state <= INIT;
+						been_ready  <= 1'b0;
+						been_extend <= 1'b0;
+						been_break  <= 1'b0;
+						key <= 10'b0_0_0000_0000;
+    				end
+    		endcase
+    	end
+    end
+    
+    always @ (posedge clk, posedge rst) begin
+    	if (rst) begin
+    		key_valid <= 1'b0;
+    		key_down <= 511'b0;
+    	end else if (key_decode[last_change] && pulse_been_ready) begin
+    		key_valid <= 1'b1;
+    		if (key[8] == 0) begin
+    			key_down <= key_down | key_decode;
+    		end else begin
+    			key_down <= key_down & (~key_decode);
+    		end
+    	end else begin
+    		key_valid <= 1'b0;
+			key_down <= key_down;
+    	end
+    end
+
+endmodule
+
+module speaker_control(
+    input clk,  // clock from the crystal
+    input rst,  // active high reset
+    input [15:0] audio_in_left, // left channel audio data input
+    input [15:0] audio_in_right, // right channel audio data input
+    output audio_mclk, // master clock
+    output audio_lrck, // left-right clock, Word Select clock, or sample rate clock
+    output audio_sck, // serial clock
+    output reg audio_sdin // serial audio data input
+); 
+
+    // Declare internal signal nodes 
+    wire [8:0] clk_cnt_next;
+    reg [8:0] clk_cnt;
+    reg [15:0] audio_left, audio_right;
+
+    // Counter for the clock divider
+    assign clk_cnt_next = clk_cnt + 1'b1;
+
+    always @(posedge clk or posedge rst)
+        if (rst == 1'b1)
+            clk_cnt <= 9'd0;
+        else
+            clk_cnt <= clk_cnt_next;
+
+    // Assign divided clock output
+    assign audio_mclk = clk_cnt[1];
+    assign audio_lrck = clk_cnt[8];
+    assign audio_sck = 1'b1; // use internal serial clock mode
+
+    // audio input data buffer
+    always @(posedge clk_cnt[8] or posedge rst)
+        if (rst == 1'b1)
+            begin
+                audio_left <= 16'd0;
+                audio_right <= 16'd0;
+            end
+        else
+            begin
+                audio_left <= audio_in_left;
+                audio_right <= audio_in_right;
+            end
+
+    always @*
+        case (clk_cnt[8:4])
+            5'b00000: audio_sdin = audio_right[0];
+            5'b00001: audio_sdin = audio_left[15];
+            5'b00010: audio_sdin = audio_left[14];
+            5'b00011: audio_sdin = audio_left[13];
+            5'b00100: audio_sdin = audio_left[12];
+            5'b00101: audio_sdin = audio_left[11];
+            5'b00110: audio_sdin = audio_left[10];
+            5'b00111: audio_sdin = audio_left[9];
+            5'b01000: audio_sdin = audio_left[8];
+            5'b01001: audio_sdin = audio_left[7];
+            5'b01010: audio_sdin = audio_left[6];
+            5'b01011: audio_sdin = audio_left[5];
+            5'b01100: audio_sdin = audio_left[4];
+            5'b01101: audio_sdin = audio_left[3];
+            5'b01110: audio_sdin = audio_left[2];
+            5'b01111: audio_sdin = audio_left[1];
+            5'b10000: audio_sdin = audio_left[0];
+            5'b10001: audio_sdin = audio_right[15];
+            5'b10010: audio_sdin = audio_right[14];
+            5'b10011: audio_sdin = audio_right[13];
+            5'b10100: audio_sdin = audio_right[12];
+            5'b10101: audio_sdin = audio_right[11];
+            5'b10110: audio_sdin = audio_right[10];
+            5'b10111: audio_sdin = audio_right[9];
+            5'b11000: audio_sdin = audio_right[8];
+            5'b11001: audio_sdin = audio_right[7];
+            5'b11010: audio_sdin = audio_right[6];
+            5'b11011: audio_sdin = audio_right[5];
+            5'b11100: audio_sdin = audio_right[4];
+            5'b11101: audio_sdin = audio_right[3];
+            5'b11110: audio_sdin = audio_right[2];
+            5'b11111: audio_sdin = audio_right[1];
+            default: audio_sdin = 1'b0;
+        endcase
+
+endmodule
