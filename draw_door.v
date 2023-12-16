829a66d3f1903af65f89fc23150f308a818f2da4
yuttttting <tina920921@gmail.com>
2023-12-16 15:48:55 +0800

    door findhint obj

--- draw_door.v
+++ draw_door.v
@@ -7,3 +7,39 @@
 
 `define rec_h = 20;
 `define rec_v = 20;
+module draw_door(
+    input [3:0] state,
+    input [9:0] h_cnt,
+    input [9:0] v_cnt,
+    input isLocked,
+    output [16:0] pixel_addr,
+    output isObject
+);
+parameter [3:0] TITLE = 0, STAFF = 1;
+parameter [3:0] STAGE1 = 2, SUCCESS1 = 3;
+parameter [3:0] STAGE2 = 4, SUCCESS2 = 5;
+parameter [3:0] STAGE3 = 6, SUCCESS3 = 7, FAIL = 8;
+wire [8:0] x,y;
+assign x = h_cnt<<1;
+assign y = v_cnt<<1;
+always@(*)begin
+    case(state)
+    STAGE1:begin
+        if(x >= 260 && x <280)begin
+            if(y>=120&&y<140)begin
+                if(isLocked)begin
+                    pixel_addr = (x -140+(y -40)*320)%76800;
+                    isObject = 1;
+                end
+                else begin
+                    pixel_addr = (x -120+(y -40)*320)%76800;
+                    isObject = 1;
+                end
+            end
+        end//lock 
+        else isObject = 0;
+    end
+    default:isObject = 0;
+    endcase
+end
+endmodule
--- draw_obj.v
+++ draw_obj.v
@@ -1,4 +1,4 @@
-// key & light & life
+// key & light
 
 `define key_h = 0;
 `define key_v = 80;
@@ -11,3 +11,52 @@
 
 `define rec_h = 20;
 `define rec_v = 20;
+
+module draw_obj(
+    input [3:0] state,
+    input [9:0] h_cnt,
+    input [9:0] v_cnt,
+    input [] todo,
+    output [16:0] pixel_addr,
+    output isObject
+);
+parameter [3:0] TITLE = 0, STAFF = 1;
+parameter [3:0] STAGE1 = 2, SUCCESS1 = 3;
+parameter [3:0] STAGE2 = 4, SUCCESS2 = 5;
+parameter [3:0] STAGE3 = 6, SUCCESS3 = 7, FAIL = 8;
+wire [8:0] x,y;
+assign x = h_cnt<<1;
+assign y = v_cnt<<1;
+always@(*)begin
+    case(state)
+    STAGE1:begin
+        if(todo ==)begin
+            if(x >=65 && x <85)begin
+                if(y >=35&& y <55)begin
+                    pixel_addr = (x -65+(y +45)*320)%76800;
+                    isObject = 1;
+                end
+            end//key1
+        end
+        else if(todo ==)begin
+            if(x >=235 && x <255)begin
+                if(y >=35&& y <55)begin
+                    pixel_addr = (x -235+(y +45)*320)%76800;
+                    isObject = 1;
+                end
+            end//key2
+        end
+        if(todo ==)begin
+            if(x >=235 && x <255)begin
+                if(y >=205&& y <225)begin
+                    pixel_addr = (x -235+(y -125)*320)%76800;
+                    isObject = 1;
+                end
+            end//key3
+        end
+        else isObject = 0;
+    end
+    default : isObject = 0;
+    endcase
+end
+endmodule
